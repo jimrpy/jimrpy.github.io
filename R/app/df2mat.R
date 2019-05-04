c7950cdb3bb9488598ece6f9a9a8ea8c03884701
jimrpy <jimrpy@gmail.com>
2019-05-04 20:47:47 +0800

    Create df2mat.R

---
+++ R/app/df2mat.R
@@ -0,0 +1,21 @@
+df2max <- function(df, var.col, var.col, var.value){
+  ## This function is for generating matrix from data.frame by given row and col
+  ## Author: LI Jinming
+  
+  ## attach packages
+  library(reshape2)
+  library(dplyr)
+  library(rlang)
+  
+  ## capture user-supplied expressions
+  var.col <- enquo(var.col)
+  var.row <- enquo(var.col)
+  var.value <- enquo(var.value)
+  ## formula, y ~ x, column ~ row
+  formula = as.formula(paste(quo_text(var.col), "~ ", quo_text(var.row)))
+
+  ## convert df to matrix
+  df %>% dcast(formula = formula, value.var = quo_text(var.value)) %>% ## convert long format to wide format
+    column_to_rownames(var = quo_text(var.col)) %>%                    ## column to rowname
+    as.matrix()                                                        ## convert to matrix
+}
